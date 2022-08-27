18f59da068f64232c47003446481649c052dd758
Belema <tamunobelemaachego@gmail.com>
2022-08-27 15:14:45 +0100

    .

--- src/components/ScrollToTop/ScrollToTop.js
+++ src/components/ScrollToTop/ScrollToTop.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import cs from './ScrollToTop.module.css'
-import { useState, useEffect } from 'react'
 import { animateScroll as scroll } from 'react-scroll'
 
 const ScrollToTop = () => {
